b2e3fbbc6320f3e1197b967e1ab4e42d3fd43356
Joan Marie Verba <verba001@umn.edu>
2018-04-27 15:24:00 -0500

    word.js

--- word.js
+++ word.js
@@ -4,29 +4,49 @@ const Letter = require("./letter.js");
 // When we use the 'extends' keyword in our class declation, this class inherits 
 // the methods and properties from the class it extends 
 class Word {
-
     constructor(word) {
-
-        // New properties
+        
+        // properties
         this.wordArray = word.split('');
-        console.log(this.wordArray);
-        this.firstLetter = new Letter(this.wordArray[0], true);
-       console.log("firstletter ", this.firstLetter);
-        this.firstLetter.printToScreen("z");
-
+        console.log("wordArray ", this.wordArray);
+        this.wordObjectArray = [];
+        for (let i=0; i < word.length; i++){
+            this.currentLetter = new Letter(this.wordArray[i], true);
+            console.log ("this current Letter ", this.currentLetter);
+            this.wordObjectArray.push(new Letter(this.wordArray[i], true));
+        };
+        console.log("wordObjectArray ", this.wordObjectArray);
     };
 
-    // New method  
+    // returns a string representing the word
     outputString(guessedLetter) {
+        let stringOnScreen = "";
+        for(let i = 0; i < this.wordObjectArray.length; i++) {
+            stringOnScreen = stringOnScreen + this.wordObjectArray[i].printToScreen(guessedLetter) + " ";
+        };
+       console.log("stringOnScreen ", stringOnScreen);
+    };
 
-        let outputArray = this.wordArray.map(function(elem){
-            
-            elem.printToScreen(guessedLetter);
-        });
-       console.log("outputArray ", outputArray);
+    //takes a character as an argument and calls the check function on each letter object
+    processCharacter(char){
+        for (let i = 0; i < this.wordObjectArray.length; i++) {
+            this.wordObjectArray[i].charCheck(char);
+        };
     };
 
 };
 
 let output = new Word("name");
-output.outputString();
+output.outputString("z");
+output.outputString("a");
+
+// this.firstLetter = new Letter(this.wordArray[0], true);
+// console.log("firstletter ", this.firstLetter);
+// this.firstLetter.printToScreen("z");
+
+// outputString(guessedLetter) {
+//     let outputArray = this.wordObjectArray.map(function (elem) {
+//         elem.printToScreen(guessedLetter);
+//     });
+//     console.log("outputArray ", outputArray);
+// };
